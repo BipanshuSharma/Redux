4dbb96f2c99bc29839335d8b860ac11225503ea5
Bipanshu <bipanshusharma7@gmail.com>
2026-06-30 19:33:28 +0530

    Project Setup Done

---
+++ redux-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl bg-black text-white h-screen w-full'>
+      <h1>hlo</h1>
+    </div>
+  )
+}
+
+export default App
