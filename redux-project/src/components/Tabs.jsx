d85a8c11486d2bd13774643faa03f634105d8d8a
Bipanshu <bipanshusharma7@gmail.com>
2026-07-10 22:48:17 +0530

    Ui bg change

--- redux-project/src/components/Tabs.jsx
+++ redux-project/src/components/Tabs.jsx
@@ -5,7 +5,7 @@ const Tabs = () => {
   return (
     <div className='flex justify-center gap-4 mt-4'>
         {tabs.map(function(elem,idx){
-            return <button className='bg-pink-900 m-2 p-2 hover:bg-pink-950 uppercase font-bold active:scale-95 cursor-pointer rounded-2xl' key={idx}>{elem}</button>
+            return <button className='bg-pink-950 m-2 p-2 hover:bg-pink-950 uppercase font-bold active:scale-95 cursor-pointer rounded-2xl' key={idx}>{elem}</button>
         })}
     </div>
   )
