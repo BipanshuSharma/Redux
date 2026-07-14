ccacb2432a5a25524f0a5c8c7b238a0dff83aad2
Bipanshu <bipanshusharma7@gmail.com>
2026-07-14 12:54:40 +0530

    Tabs Done

--- redux-project/src/components/Tabs.jsx
+++ redux-project/src/components/Tabs.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { setActiveTab } from '../redux/features/searchSlice';
 
 const Tabs = () => {
-    const tabs = ['photo', 'video'];
+  const tabs = ['photo', 'video'];
+  const dispatch = useDispatch();
+  const activeTab = useSelector((state) => state.search.activeTab);
   return (
     <div className='flex justify-center gap-4 mt-4'>
-        {tabs.map(function(elem,idx){
-            return <button className='bg-pink-950 m-2 p-2 hover:bg-pink-950 uppercase font-bold active:scale-95 cursor-pointer rounded-2xl' key={idx}>{elem}</button>
-        })}
+      {tabs.map(function (elem, idx) {
+        return (
+          <button className={`${activeTab==elem ? "bg-blue-700" : "bg-pink-950"} m-2 p-2 uppercase font-bold active:scale-95 cursor-pointer rounded-2xl`}
+            onClick={() => dispatch(setActiveTab(elem))}
+            key={idx}>{elem}</button>
+        )
+      })}
     </div>
   )
 }
